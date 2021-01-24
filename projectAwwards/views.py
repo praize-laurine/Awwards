f99e081aabad454b78d9f4a451fce13b6237236d
praize-laurine <preyzelaurine@gmail.com>
2021-01-24 12:15:04 +0300

    created a view function for signup form

--- projectAwwards/views.py
+++ projectAwwards/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+# Create your views fhere.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username,password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return redirect(request,'registration/signUp_form.html' {'form':form})            
+
+
