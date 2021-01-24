f37105bf11a2bc574ed45b58637a401b23de0303
praize-laurine <preyzelaurine@gmail.com>
2021-01-24 19:57:08 +0300

    created a profile model

--- projectAwwards/models.py
+++ projectAwwards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 
 # Create your models here.
@@ -31,3 +32,28 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to='profile/', blank ='true',default='default.png'))
+    bio = models.TextField()
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    name = models.CharField(blank=True, max_length=120)
+    date_created = models.DateField(auto_now_add = True)      
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    def save_profile(self):
+        self.save
+    
+    def delete_user(self):
+        self.delete()
+
+@receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)     
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()           
